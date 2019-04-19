09d7b2dd57ded342dd50b0ab8d80c0010eedb5cb
Chris Acker <christopheracker1@gmail.com>
2019-04-19 19:27:35 -0400

    printn to csv

--- wamo.py
+++ wamo.py
@@ -2,7 +2,7 @@
 import pandas_datareader.data as web
 from datetime import datetime
 
-start = datetime(2015, 2, 9)
-end = datetime(2019, 4, 19)
+start = datetime(2019, 4, 18)
+end = datetime(2019, 4, 18)
 f = web.DataReader('COF', 'iex', start, end)
-print(f)
+f.to_csv(index=False, path_or_buf='training_data.csv')
