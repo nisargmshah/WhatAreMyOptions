d0d947407fd9c680fafb276de359f418dadf361f
Nisarg Shah <nisarg1shah@gmail.com>
2019-04-19 19:01:57 -0400

    Obtaining Stock Data via Pandas Datareader

--- wamo.py
+++ wamo.py
@@ -1,1 +1,8 @@
 # predictor
+import pandas_datareader.data as web
+from datetime import datetime
+
+start = datetime(2015, 2, 9)
+end = datetime(2019, 4, 19)
+f = web.DataReader('COF', 'iex', start, end)
+print(f)
