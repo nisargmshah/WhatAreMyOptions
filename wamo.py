6f8f5d40063898085089b6dfd36e6dc0d1cbe6d6
Nisarg Shah <nisarg1shah@gmail.com>
2019-04-19 20:29:17 -0400

    Added More to Jupyter Notebook

--- wamo.py
+++ wamo.py
@@ -4,5 +4,5 @@ from datetime import datetime
 
 start = datetime(2019, 4, 18)
 end = datetime(2019, 4, 18)
-f = web.DataReader('COF', 'iex', start, end)
+f = web.DataReader('FIT', 'iex', start, end)
 f.to_csv(index=False, path_or_buf='training_data.csv')
